08c256225e8a34d1d8d5f30dd3ed099a2fc24a36
khai520 <45537943+khai520@users.noreply.github.com>
2023-11-08 12:17:14 +0700

    Add files via upload

---
+++ QuanlyDuAn/DoiTac/Home.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DoiTac
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListViewItem listViewItem1 = new ListViewItem("Id");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listViewItem1.Checked = true;
+            listViewItem1.StateImageIndex = 1;
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem1 });
+            listView1.Location = new Point(12, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(938, 280);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(962, 538);
+            Controls.Add(listView1);
+            Name = "Home";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listView1;
+    }
+}
---
+++ QuanlyDuAn/DoiTac/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoiTac
+{
+    public partial class Login : Form
+    {
+        public Login(string text)
+        {
+            InitializeComponent();
+            this.txt_TenDn.Text = text;
+        }
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_DN_Click(object sender, EventArgs e)
+        {
+            lb_TB.Show();
+        }
+
+        private void btn_DK_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Register rg = new Register(txt_TenDn.Text);
+            rg.ShowDialog();
+            this.Close();
+        }
+
+    }
+}
