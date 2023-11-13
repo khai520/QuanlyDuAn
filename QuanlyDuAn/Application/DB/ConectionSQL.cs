975fd7444f7e4ee60aadc97fb75952b7d867ea4c
khai520 <khai5108@gmail.com>
2023-11-13 15:02:46 +0700

    last

---
+++ QuanlyDuAn/Application/DB/ConectionSQL.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDuAnBDS.DB
+{
+    internal class ConectionSQL
+    {
+        SqlConnection conn; //Hỗ trợ kết nối
+        SqlCommand com; // Hỗ trợ xử lý lệnh
+        SqlDataAdapter da; //adapter
+        DataSet ds; // anh bang du lieu
+        public void Ketnoi()
+        {
+            // chuỗi kết nối csdl
+            string s = "Data Source =.; Initial Catalog = QLBDS; Integrated Security = True";
+            conn = new SqlConnection(s); //  khởi tạo kết nối
+            da = new SqlDataAdapter(); // khởi tạo bộ điều phối dữ liệu
+            ds = new DataSet(); // khởi tạo ảnh của cơ sở dữ liệu
+        }
+        public void getDataDSDD(ListView lv)
+        {
+            lv.Items.Clear();
+            ds.Reset();
+            com = new SqlCommand("select * from DuAnDaDuyet ", conn);// Câu lệnh truy vấn
+            da.SelectCommand = com; // select
+            da.Fill(ds, "DuAnDaDuyet"); // điền dữ liệu vào dataset
+            for (int j = 0; j < ds.Tables[0].Rows.Count; j++)
+            {
+                lv.Items.Add(Convert.ToString(j + 1));
+                for (int i = 0; i < ds.Tables[0].Columns.Count; i++)
+                {
+                    lv.Items[j].SubItems.Add(ds.Tables[0].Rows[j].ItemArray[i].ToString());
+                }
+            }
+        }
+        public string getDataTk(string tdn, string mk)
+        {
+            ds.Reset();
+            com = new SqlCommand($"select Chucdanh from TkDangNhap where Tdn = '{tdn}' and Mk = '{mk}'", conn);
+            da.SelectCommand = com;
+            da.Fill(ds, "TkDangNhap");
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                if (ds.Tables[0].Rows[0].ItemArray[0].ToString() == "DoiTac")
+                {
+                    return "DT";
+                }
+                else
+                {
+                    return "Null";
+                }
+            }
+            else
+            {
+                return "Null";
+            }
+        }
+    }
+}
---
+++ QuanlyDuAn/Application/Log/Register.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoiTac
+{
+
+    public partial class Register : Form
+    {
+
+        public Register(string textbox)
+        {
+            InitializeComponent();
+            this.txt_TenDn.Text = textbox;
+        }
+
+        private void btn_Dn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login lg = new Login(txt_TenDn.Text);
+            lg.ShowDialog();
+            this.Close();
+        }
+        public bool KiemtraMk(string mk, string xnmk)
+        {
+            if (mk != xnmk)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void btn_Dk_Click(object sender, EventArgs e)
+        {
+            if (KiemtraMk(lb_Mk.Text, lb_XnMk.Text))
+            {
+
+            }
+        }
+
+
+    }
+}
